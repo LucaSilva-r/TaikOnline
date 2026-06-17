d8a1b0e21a241550a11d66cf8aac67304ae8760e
Luca Silva <silva.luca.job@gmail.com>
2026-06-17 22:55:27 +0200

    added safeguards to accidental db wipes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,17 @@ use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        if (file_exists(dirname(__DIR__).'/bootstrap/cache/config.php')) {
+            throw new \RuntimeException(
+                "Testing with cached configuration is disabled to prevent accidental data loss in your development/production database. Run 'php artisan config:clear' first."
+            );
+        }
+
+        parent::setUp();
+    }
+
     protected function skipUnlessFortifyHas(string $feature, ?string $message = null): void
     {
         if (! Features::enabled($feature)) {
